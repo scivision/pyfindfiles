6320c87e571cc877b23a913f0db9f1b8339d4b76
Michael Hirsch <scivision@users.noreply.github.com>
2020-10-01 23:04:54 -0400

    remove unused code

--- src/pyfindfiles/__init__.py
+++ src/pyfindfiles/__init__.py
@@ -1,3 +1,9 @@
+import os
+import asyncio
+
 from .text import findtext
 from .vid import findvid, findvid_gnu
 from .project import detect_lang
+
+if os.name == "nt":
+    asyncio.set_event_loop_policy(asyncio.WindowsProactorEventLoopPolicy())  # type: ignore
